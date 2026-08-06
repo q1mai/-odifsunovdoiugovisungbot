3e7c746ff198bfe8c54d247f4c5d55687ea61a52
Кирилл <q1mai@MacBook-Air-Kirill.local>
2026-08-06 11:55:41 +0300

    кот на скейте + savemymessages

--- tgbot.py
+++ tgbot.py
@@ -17,9 +17,14 @@ app = Flask(__name__)
 def start(message):
     bot.send_message(message.chat.id, "Привет! Я бот на вебхуке.")
 
+@bot.message_handler(func=lambda message: 'ты че кот на скейте' in message.text)
+def kot(message):
+    bot.send_photo(message.chat_id, 'https://www.meme-arsenal.com/create/chose?tag=%D0%A2%D0%B5%D1%80%D0%BF%D0%B8%D0%BB%D0%B0')
+    bot.send_message(message.chat_id, 'не пиши сюда больше')
+
 GROUP_CHAT_ID = -5363411318
 
-@bot.message_handler(func=lambda message: True)
+@bot.message_handler(commands=['savemymessages'])
 def forward_to_group(message):
     chat_id = message.chat.id
     user = message.from_user
@@ -33,7 +38,8 @@ def forward_to_group(message):
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     print(f"Получено сообщение: {message.text} от {message.chat.id}")
-    bot.reply_to(message, f"Ты написал: {message.text}")
+    bot.reply_to(message, "Ты написал: {message.text}")
+
 
 @app.route(f'/{TOKEN}', methods=['POST'])
 def webhook():

9ba3bf5bf7825e4d8a85f94280ab75f4a2d7863a
Кирилл <q1mai@MacBook-Air-Kirill.local>
2026-08-06 10:54:36 +0300

    убрал message после reply_to в start

--- tgbot.py
+++ tgbot.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 @bot.message_handler(commands=['start'])
 def start(message):
     print(f"Получена команда /start от {message.chat.id}")
-    bot.reply_to(message, "Привет! Я бот на вебхуке.")
+    bot.reply_to("Привет! Я бот на вебхуке.")
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
